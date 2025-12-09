614f23931ecbc16ac761c6ddadbe88e55f9c6442
Samiul Haque Ruf <rufsamiul1288@gmail.com>
2025-12-09 23:41:53 +0600

    fix warnings in index.js and update dependencies

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import "./index.css";
 
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { TicketsProvider } from "./context/TicketsContext.jsx";
-import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
